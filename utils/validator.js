99752293735c661bb7331cd4001f7763935fad24
5464974af1157cf859095efbe671f47 <skills2035@gmail.com>
2026-01-07 17:17:29 +0900

    feat: Add input validation utilities

---
+++ utils/validator.js
@@ -0,0 +1,16 @@
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const validatePassword = (password) => {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  return passwordRegex.test(password);
+};
+
+const validateUsername = (username) => {
+  const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+  return usernameRegex.test(username);
+};
+
+module.exports = { validateEmail, validatePassword, validateUsername };
