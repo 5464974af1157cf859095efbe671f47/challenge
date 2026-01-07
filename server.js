391c00d6a8b9e88cfe777b2fcdc4b6251fc5b691
5464974af1157cf859095efbe671f47 <skills2035@gmail.com>
2026-01-07 17:17:42 +0900

    refactor: Add profile routes and error handler to server

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ const express = require('express');
 const config = require('./config/config');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
+const profileRoutes = require('./routes/profile');
 const { authenticateToken } = require('./middleware/auth');
+const { errorHandler } = require('./utils/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Secure API' });
+  res.json({ 
+    message: 'Welcome to the Secure API',
+    version: '1.0.0'
+  });
 });
 
 app.use('/auth', authRoutes);
 
 app.use('/users', authenticateToken, userRoutes);
+app.use('/profile', authenticateToken, profileRoutes);
+
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
+
+module.exports = app;
